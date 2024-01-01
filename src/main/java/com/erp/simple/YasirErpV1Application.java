da61a4f2b3d28c68581b5f9b6f8213722fea5cba
yasirmaulana <yasir.maulana@gmail.com>
2024-01-01 08:21:46 +0700

    first init

---
+++ src/main/java/com/erp/simple/YasirErpV1Application.java
@@ -0,0 +1,13 @@
+package com.erp.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YasirErpV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YasirErpV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/erp/simple/YasirErpV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YasirErpV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
